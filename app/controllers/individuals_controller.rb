896b1cc6772ab91495843b4ba5d4c812636832b0
Mikelle Brown <mikellebrown22@gmail.com>
2019-08-20 19:50:21 -0600

    another commit

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -12,24 +12,46 @@ class IndividualsController < ApplicationController
   end
 
   def create
-    @individual = Individual.new(pages_params)
+    @individual = Individual.new(individuals_params)
  
     if @individual.save
       redirect_to individuals_path
     else
       render :new
     end
-
-  def destroy
-    @individual = Individual.find(params[:id])
-    @individual.destroy
-      redirect_to individuals_path
-    end
     def edit
-      @individual = Individual.find params[:id]
-      @individual.individuals.build
-   end
- 
+      @individual = Individual.find(individuals[:id])
+    end
+    def update
+      raise params.inspect
+    end
+    def update
+      @individual = Individual.find(params[:id])
+      @individual.update
+      redirect_to individual_path(@individual)
+    end
+
+    def destroy
+      def destroy
+        @individual = individual.find params[:id]
+        @individual.destroy
+        redirect_to individuals_path
+      end
+    end
+
+    
+    
+    
+    
+    
+    # @individual = Individual.find(params[:id])
+    # @individual.destroy
+    #   redirect_to individuals_path
+
+    # def edit
+    #   @individual = Individual.edit
+    #   redirect_to individuals_path
+  
   private
  
   def individuals_params
@@ -38,4 +60,5 @@ class IndividualsController < ApplicationController
     end
   end
 end
+
   
